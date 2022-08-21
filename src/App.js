0b9ee042664f753519627ef0088ba410d3c3049f
hyparh <hyparh@gmail.com>
2022-08-21 13:36:23 +0300

    Settings button now accepts theme color

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {
 import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
-  const { activeMenu, themeSettings , setThemeSettings } = useStateContext();
+  const { activeMenu, themeSettings , setThemeSettings, currentColor } = useStateContext();
 
   return (
     <div>
@@ -24,7 +24,7 @@ const App = () => {
               <button type="button" className="text-3xl 
               p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
                 onClick={() => setThemeSettings(true)}
-                style={{ background: 'blue', borderRadius: '50%' }}>
+                style={{ background: currentColor, borderRadius: '50%' }}>
                 <FiSettings />
               </button>
             </TooltipComponent>
